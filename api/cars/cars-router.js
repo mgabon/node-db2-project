fe949d1631c59deae7fd31a0378a70954ba5351a
Mark <markgabon@hotmail.com>
2022-02-15 22:44:04 -0500

    pow

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,34 @@
 // DO YOUR MAGIC
+const express = require('express')
+const Cars = require('./cars-model')
+const {checkCarId, checkCarPayload, checkVinNumberValid, checkVinNumberUnique } = require('./cars-middleware')
+const router = express.Router();
+
+router.get('/', async (req, res, next) => {
+try {
+const car = await Cars.getAll()
+res.json(car)
+} catch (err) {
+    next(err)
+}
+})
+
+router.get('/:id', checkCarId, async (req, res, next) => {
+    try {
+        const car = await Cars.getAll()
+        res.json(car)
+        } catch (err) {
+            next(err)
+        }
+    })
+
+    router.post('/', checkCarPayload, checkVinNumberValid, checkVinNumberUnique, async (req, res, next) => {
+        try {
+            const car = await Cars.create(req.body)
+            res.json(car)
+            } catch (err) {
+                next(err)
+            }
+        })
+        
+module.exports = router        
